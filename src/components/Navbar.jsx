446f1761edc1b00bfb4c368f0a865dac0763c727
Rahul Soni <your_github_rahulsoni66676@gmail.com>
2026-03-24 18:41:07 +0530

    Final Update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,11 +14,11 @@ function Navbar({ cart = [], wishlist = [], searchTerm, setSearchTerm }) {
   return (
     <nav className="navbar navbar-expand-lg bg-white sticky-top">
       <div className="container">
-        <Link className="navbar-brand fw-bold text-primary fs-2" to="/">
+        <Link className="navbar-brand fw-bold text-primary fs-2 me-3" to="/">
           ShopEasy
         </Link>
 
-        <form className="d-flex flex-grow-1 mx-lg-4 my-2 my-lg-0" onSubmit={onSearch}>
+        <form className="d-flex flex-grow-1 my-2 my-lg-0 me-3" onSubmit={onSearch}>
           <input
             className="form-control me-2"
             type="search"
@@ -31,7 +31,7 @@ function Navbar({ cart = [], wishlist = [], searchTerm, setSearchTerm }) {
           </button>
         </form>
 
-        <div className="d-flex gap-2">
+        <div className="d-flex align-items-center gap-2">
           <Link to="/cart" className="btn btn-outline-primary position-relative">
             Cart
             {cartCount > 0 && (
